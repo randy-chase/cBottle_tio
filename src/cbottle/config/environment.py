dec3c4e9479cd78ba69ee7868ea0b3ef94029eac
Noah D. Brenowitz <nbrenowitz@nvidia.com>
2025-05-28 12:08:18 -0700

    remove system specific paths from config

--- src/cbottle/config/environment.py
+++ src/cbottle/config/environment.py
@@ -19,16 +19,13 @@ dotenv.load_dotenv(dotenv.find_dotenv(usecwd=True))
 
 CACHE_DIR = os.path.expanduser("~/.cache/cbottle")
 
-#############
-# ERA5 inputs
-#############
-V6_ERA5_ZARR = os.getenv(
-    "V6_ERA5_ZARR", "/global/cfs/cdirs/trn006/data/nvidia/era5_hpx_6.zarr/"
-)
+###############
+# ERA5 inputs #
+###############
+V6_ERA5_ZARR = os.getenv("V6_ERA5_ZARR", "")
 V6_ERA5_ZARR_PROFILE = os.getenv("V6_ERA5_ZARR_PROFILE", "")
 
 # SST data from CMIP
-# Data is downloaded from the https://aims2.llnl.gov/search
 AMIP_MID_MONTH_SST = os.getenv(
     "AMIP_MID_MONTH_SST",
     os.path.join(
@@ -38,43 +35,36 @@ AMIP_MID_MONTH_SST = os.getenv(
 )
 AMIP_MID_MONTH_SST_PROFILE = os.getenv("AMIP_MID_MONTH_SST_PROFILE", "")
 
-######
-# ICON
-######
-RAW_DATA_URL = "/global/cfs/cdirs/trn006/data/nvidia/ngc3028_PT30M_4weeks_10.zarr/"
+########
+# ICON #
+########
+RAW_DATA_URL = os.getenv("RAW_DATA_URL", "")
 RAW_DATA_PROFILE = os.getenv("RAW_DATA_PROFILE", "")
-RAW_DATA_URL_6 = os.getenv(
-    "RAW_DATA_URL_6", "/global/cfs/cdirs/trn006/data/nvidia/ngc3028_PT30M_6.zarr/"
-)
+RAW_DATA_URL_6 = os.getenv("RAW_DATA_URL_6", "")
 
-V6_ICON_ZARR = os.getenv(
-    "V6_ICON_ZARR", "/global/cfs/cdirs/trn006/data/nvidia/ICON_v6_dataset.zarr/"
-)
-
+V6_ICON_ZARR = os.getenv("V6_ICON_ZARR", "")
 V6_ICON_ZARR_PROFILE = os.getenv("V6_ICON_ZARR_PROFILE", "")
 
 LAND_DATA_URL_10 = os.getenv(
     "LAND_DATA_URL_10",
-    "/global/cfs/cdirs/trn006/data/nvidia/landfraction/ngc3028_P1D_10.zarr/",
+    "",
 )
 LAND_DATA_URL_6 = os.getenv(
     "LAND_DATA_URL_6",
-    "/global/cfs/cdirs/trn006/data/nvidia/landfraction/ngc3028_P1D_6.zarr/",
+    "",
 )
 LAND_DATA_PROFILE = os.getenv("LAND_DATA_PROFILE", "")
 
 
 SST_MONMEAN_DATA_URL_6 = os.getenv(
     "SST_MONMEAN_DATA_URL_6",
-    "/global/cfs/cdirs/trn006/data/nvidia/ngc3028_P1D_ts_monmean_6.zarr",
+    "",
 )
 SST_MONMEAN_DATA_PROFILE = os.getenv("SST_MONMEAN_DATA_PROFILE", "")
 
 
 # project file
-PROJECT_ROOT = os.getenv(
-    "PROJECT_ROOT", "/global/cfs/cdirs/trn006/data/nvidia/cBottle/"
-)
+PROJECT_ROOT = os.getenv("PROJECT_ROOT", "")
 DATA_ROOT = os.getenv("DATA_ROOT", os.path.join(PROJECT_ROOT, "datasets"))
 CHECKPOINT_ROOT = os.getenv(
     "CHECKPOINT_ROOT", os.path.join(PROJECT_ROOT, "training-runs")
